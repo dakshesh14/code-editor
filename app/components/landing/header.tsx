3fd09f242d314a80b82164bef6320bb27ae71a85
Dakshesh Jain <dakshesh.jain14@gmail.com>
2023-07-29 15:32:06 +0530

    style: landing page

---
+++ app/components/landing/header.tsx
@@ -0,0 +1,124 @@
+import { useState } from "react";
+
+// next
+import Link from "next/link";
+import Image from "next/image";
+
+// headless ui
+import { Dialog } from "@headlessui/react";
+
+// heroicons
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const navigation = [
+  {
+    name: "New Project",
+    href: "/project/new",
+  },
+  {
+    name: "About me",
+    href: "https://dakshesh.me",
+    target: "_blank",
+  },
+  {
+    name: "Source Code",
+    href: "https://github.com/dakshesh14/code-editor",
+    target: "_blank",
+  },
+];
+
+export const Header = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <header className="absolute inset-x-0 top-0 z-50">
+      <nav
+        className="flex items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <Link href="/" className="-m-1.5 p-1.5">
+            <span className="sr-only">Code Editor</span>
+            <div className="h-12 w-12 relative">
+              <Image
+                alt="Code Editor"
+                fill
+                className="object-contain"
+                src="/logo.png"
+              />
+            </div>
+          </Link>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          {navigation.map((item) => (
+            <Link
+              key={item.name}
+              href={item.href}
+              target={item.target}
+              className="text-sm font-semibold leading-6 text-white"
+            >
+              {item.name}
+            </Link>
+          ))}
+        </div>
+      </nav>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-50" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
+          <div className="flex items-center justify-between">
+            <Link href="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">Code Editor</span>
+              <div className="h-12 w-12 relative">
+                <Image
+                  alt="Code Editor"
+                  fill
+                  className="object-contain"
+                  src="/logo.png"
+                />
+              </div>
+            </Link>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-400"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/25">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    target={item.target}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800"
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  );
+};
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,3 +1,62 @@
-export default function Home() {
-  return <h1>Hello World 👋</h1>;
-}
+import Link from "next/link";
+
+// components
+import { Header } from "@/components/landing";
+
+const Landing = () => {
+  return (
+    <div className="bg-gray-900">
+      <Header />
+
+      <div className="relative isolate pt-14">
+        <div className="py-24 sm:py-32 lg:pb-40">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl text-center">
+              <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                Write & execute code collaboratively
+              </h1>
+              <p className="mt-6 text-lg leading-8 text-gray-300">
+                A project powered by Django, Next.js, Tailwind, WebSocket, CRDT,
+                docker and more.
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6">
+                <Link
+                  href="/projects/new"
+                  className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+                >
+                  Create Project
+                </Link>
+                <Link
+                  target="_blank"
+                  href="https://www.dakshesh.me"
+                  className="text-sm font-semibold leading-6 text-white"
+                >
+                  About me <span aria-hidden="true">→</span>
+                </Link>
+              </div>
+            </div>
+
+            <div className="flex relative-md mt-16 sm:mt-24 w-full h-[40rem] bg-red-50 justify-center items-center p-5">
+              <p>Please assume this is a photo of the final product :P</p>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
